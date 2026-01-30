a8510f653373ebfb7a91817381c60ec7611687b3
anko9801 <37263451+anko9801@users.noreply.github.com>
2026-01-30 16:33:31 +0900

    refactor: merge syncthing into services/default.nix
    - Remove separate syncthing.nix (only one service)
    - Simplify services module structure

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,7 +1,18 @@
-_:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-  imports = [
-    ./syncthing.nix
-  ];
+  # Syncthing - file synchronization
+  # Note: Only enable on non-WSL Linux (WSL should use Windows Syncthing)
+  services.syncthing = lib.mkIf (!(config.targets.genericLinux.enable or false)) {
+    enable = true;
+    tray.enable = false; # No tray on headless
+  };
+
+  # Install syncthing package for all platforms
+  home.packages = [ pkgs.syncthing ];
 }
