efa484ddcd594c9f91fbd2f5f900fae36967e4ed
anko9801 <37263451+anko9801@users.noreply.github.com>
2026-02-01 01:35:11 +0900

    chore: remove unused haumea input

--- flake.nix
+++ flake.nix
@@ -30,11 +30,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    haumea = {
-      url = "github:nix-community/haumea";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
