0db042bc9843b22693be58dd4ed9a62796b4556d
anko9801 <37263451+anko9801@users.noreply.github.com>
2026-02-02 21:08:26 +0900

    refactor: merge dircolors into theme/default.nix

--- theme/default.nix
+++ theme/default.nix
@@ -5,7 +5,6 @@
 }:
 
 {
-  imports = [ ./dircolors.nix ];
   options.theme.colors = lib.mkOption {
     type = lib.types.attrsOf lib.types.str;
     default = {
@@ -113,8 +112,32 @@
         fzf.enable = true;
         tmux.enable = true;
         vim.enable = true;
-        zellij.enable = false; # zellij styling can conflict
+        zellij.enable = false;
       };
+    };
+
+    programs.dircolors = {
+      enable = true;
+      enableZshIntegration = true;
+      extraConfig = ''
+        DIR 01;34
+        LINK 01;36
+        EXEC 01;32
+        .tar 01;31
+        .gz 01;31
+        .zip 01;31
+        .7z 01;31
+        .jpg 01;35
+        .png 01;35
+        .mp3 00;36
+        .mp4 00;36
+        .py 00;33
+        .js 00;33
+        .ts 00;33
+        .rs 00;33
+        .go 00;33
+        .nix 00;33
+      '';
     };
   };
 }
